f1518da35cd001e217606cfe305de1b913c149c2
Bharat D Bhadresha <bharatdb.me@gmail.com>
2021-03-29 21:51:53 +0530

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,6 @@
-console.log("test")
+import { serve } from 'https://deno.land/std/http/server.ts'
+const s = serve({ port: 8000 })
+console.log(`🦕 Deno server running at http://localhost:8000/ 🦕`)
+for await (const req of s) {
+  req.respond({ body: 'Hello from your first Deno server' })
+}
