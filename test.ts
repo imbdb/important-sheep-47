a226529a87887fcf12f0886ad99539fd81859df0
Bharat D Bhadresha <bharatdb.me@gmail.com>
2021-03-29 22:00:19 +0530

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
-import { serve } from 'https://deno.land/std/http/server.ts'
-const s = serve({ port: 8000 })
-console.log(`🦕 Deno server running at http://localhost:8000/ 🦕`)
-for await (const req of s) {
-  req.respond({ body: 'Hello from your first Deno server' })
-}
+addEventListener("fetch", (event) => {
+  const response = new Response("Hello World!", {
+    headers: { "content-type": "text/plain" },
+  });
+  event.respondWith(response);
+});
